bf8e319e18b5a1cf4d89729141e9f47dce4cc814
Joel Spadin <joelspadin@gmail.com>
2024-09-23 01:06:14 -0500

    Refactor code block, add copy button

---
+++ src/CodeBlock.tsx
@@ -0,0 +1,148 @@
+import {
+  Button,
+  makeStyles,
+  mergeClasses,
+  shorthands,
+  tokens,
+  Tooltip,
+  useTimeout,
+} from "@fluentui/react-components";
+import { HTMLAttributes, useCallback, useMemo, useState } from "react";
+
+import {
+  bundleIcon,
+  CheckmarkFilled,
+  CopyFilled,
+  CopyRegular,
+  FluentIconsProps,
+} from "@fluentui/react-icons";
+import hljs from "highlight.js";
+import dts from "highlight.js/lib/languages/dts";
+import "highlight.js/styles/github-dark.css";
+
+hljs.registerLanguage("dts", dts);
+
+const Copy = bundleIcon(CopyFilled, CopyRegular);
+
+export interface CodeBlockProps extends HTMLAttributes<HTMLPreElement> {
+  children?: string;
+  language?: string;
+}
+
+export const CodeBlock: React.FC<CodeBlockProps> = ({
+  children,
+  language,
+  className,
+  ...props
+}) => {
+  children ??= "";
+  language ??= "dts";
+
+  const classes = useStyles();
+
+  const [copied, setCopied] = useState(false);
+  const [setTimeout] = useTimeout();
+
+  const highlighted = useMemo(
+    () => hljs.highlight(children, { language }).value,
+    [children, language]
+  );
+
+  const copyCode = useCallback(() => {
+    navigator.clipboard.writeText(children);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  }, [children, setCopied, setTimeout]);
+
+  return (
+    <pre className={classes.pre}>
+      <code
+        className={mergeClasses("hljs", classes.code, className)}
+        dangerouslySetInnerHTML={{ __html: highlighted }}
+        {...props}
+      />
+      <Tooltip content="Copy" relationship="label" withArrow>
+        <Button
+          size="large"
+          appearance="subtle"
+          className={classes.copyButton}
+          icon={<CopyIcon copied={copied} />}
+          onClick={copyCode}
+        />
+      </Tooltip>
+    </pre>
+  );
+};
+
+interface CopyIconProps extends FluentIconsProps {
+  copied: boolean;
+}
+
+const CopyIcon: React.FC<CopyIconProps> = ({ copied, ...props }) => {
+  const classes = useStyles();
+
+  return (
+    <span className={classes.iconWrapper}>
+      <CheckmarkFilled
+        className={mergeClasses(
+          classes.icon,
+          classes.checkmark,
+          !copied && classes.hidden
+        )}
+        {...props}
+      />
+      <Copy
+        className={mergeClasses(classes.icon, copied && classes.hidden)}
+        {...props}
+      />
+    </span>
+  );
+};
+
+const useStyles = makeStyles({
+  pre: {
+    display: "block",
+    boxSizing: "border-box",
+    position: "relative",
+    overflow: "hidden",
+    margin: 0,
+    padding: 0,
+
+    borderRadius: tokens.borderRadiusMedium,
+    boxShadow: tokens.shadow4,
+  },
+  code: {
+    display: "block",
+    boxSizing: "border-box",
+    overflow: "auto",
+    margin: 0,
+    ...shorthands.padding(tokens.spacingVerticalM, tokens.spacingHorizontalM),
+
+    fontFamily: tokens.fontFamilyMonospace,
+    scrollbarColor: `${tokens.colorNeutralForeground3} ${tokens.colorNeutralBackground3}`,
+  },
+  copyButton: {
+    position: "absolute",
+    top: tokens.spacingVerticalS,
+    right: tokens.spacingHorizontalS,
+  },
+  checkmark: {
+    color: tokens.colorPaletteLightGreenForeground1,
+  },
+  iconWrapper: {
+    position: "relative",
+    width: "24px",
+    height: "24px",
+  },
+  icon: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    transitionProperty: "transform",
+    transitionDuration: tokens.durationSlower,
+    transitionTimingFunction: tokens.curveEasyEaseMax,
+  },
+  hidden: {
+    transform: "scale(0)",
+  },
+});
--- src/ExportPage.tsx
+++ src/ExportPage.tsx
@@ -1,17 +1,8 @@
-import {
-  makeStyles,
-  mergeClasses,
-  shorthands,
-  tokens,
-} from "@fluentui/react-components";
-import hljs from "highlight.js";
-import dts from "highlight.js/lib/languages/dts";
-import "highlight.js/styles/github-dark.css";
+import { makeStyles, tokens } from "@fluentui/react-components";
 import { useMemo } from "react";
+import { CodeBlock } from "./CodeBlock";
 import { formatLayout } from "./formatter/layout";
 import { useEditState } from "./useEditState";
-
-hljs.registerLanguage("dts", dts);
 
 export const ExportPage: React.FC = () => {
   const classes = useStyles();
@@ -19,22 +10,15 @@ export const ExportPage: React.FC = () => {
 
   const devicetree = useMemo(() => formatLayout(state), [state]);
 
-  const highlighted = useMemo(
-    () => hljs.highlight(devicetree, { language: "dts" }).value,
-    [devicetree]
-  );
-
   // TODO: add a copy button
   // TODO: add download button
   // TODO: warn if some positions are undefined
 
   return (
     <div className={classes.root}>
-      <div className={classes.wrapper}>
-        <pre className={mergeClasses("hljs", classes.textarea)}>
-          <code dangerouslySetInnerHTML={{ __html: highlighted }} />
-        </pre>
-      </div>
+      <CodeBlock language="dts" className={classes.code}>
+        {devicetree}
+      </CodeBlock>
     </div>
   );
 };
@@ -47,24 +31,11 @@ const useStyles = makeStyles({
 
     marginTop: tokens.spacingVerticalM,
   },
-  wrapper: {
-    overflow: "hidden",
-    borderRadius: tokens.borderRadiusMedium,
-    scrollbarColor: `${tokens.colorNeutralForeground3} ${tokens.colorNeutralBackground3}`,
-    boxShadow: tokens.shadow4,
-  },
-  textarea: {
-    boxSizing: "border-box",
 
+  code: {
+    boxSizing: "border-box",
     width: "800px",
     maxWidth: "calc(100vw - 48px)",
     height: `calc(100vh - 48px - ${tokens.spacingVerticalM} * 2)`,
-    maxHeight: "unset",
-    overflow: "auto",
-
-    margin: 0,
-    ...shorthands.padding(tokens.spacingVerticalM, tokens.spacingHorizontalM),
-
-    fontFamily: tokens.fontFamilyMonospace,
   },
 });
