66672649b382f71c4874b2bada3c97e79555aa2f
Joel Spadin <joelspadin@gmail.com>
2024-09-24 10:54:17 -0500

    Fix warning about relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
 import { defineConfig } from 'vite';
+
+const noop = path.resolve(__dirname, 'src/noop.ts');
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-            fs: './src/noop.ts',
-            path: './src/noop.ts',
+            fs: noop,
+            path: noop,
         },
     },
 });
