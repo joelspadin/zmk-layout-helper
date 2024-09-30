5f5b68b4cdad330411c249505619226ac3cf23ff
Joel Spadin <joelspadin@gmail.com>
2024-09-30 10:33:33 -0500

    Add links to github and other tools

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,7 +1,19 @@
-import { Tab, TabList, makeStyles, tokens } from '@fluentui/react-components';
+import {
+    Button,
+    Menu,
+    MenuItemLink,
+    MenuList,
+    MenuPopover,
+    MenuTrigger,
+    Tab,
+    TabList,
+    makeStyles,
+    tokens,
+} from '@fluentui/react-components';
 import { useState } from 'react';
 import { ContextProviders } from './ContextProviders';
 import { ExportPage } from './ExportPage';
+import { GitHubIcon } from './GithubIcon';
 import { ImportPage } from './ImportPage';
 import { PositionMapPage } from './PositionMapPage';
 import { useEditState } from './useEditState';
@@ -26,19 +38,57 @@ const PageContents: React.FC = () => {
 
     return (
         <>
-            <TabList
-                selectedValue={tab}
-                onTabSelect={(ev, data) => setTab(data.value as TabValue)}
-                className={classes.tabs}
-            >
-                <Tab value="import">Import devicetree</Tab>
-                <Tab value="positions" disabled={disabled}>
-                    Edit position map
-                </Tab>
-                <Tab value="export" disabled={disabled}>
-                    Export devicetree
-                </Tab>
-            </TabList>
+            <div className={classes.header}>
+                <div></div>
+                <TabList
+                    selectedValue={tab}
+                    onTabSelect={(ev, data) => setTab(data.value as TabValue)}
+                    className={classes.tabs}
+                >
+                    <Tab value="import">Import devicetree</Tab>
+                    <Tab value="positions" disabled={disabled}>
+                        Edit position map
+                    </Tab>
+                    <Tab value="export" disabled={disabled}>
+                        Export devicetree
+                    </Tab>
+                </TabList>
+                <div className={classes.links}>
+                    <Menu positioning="below" persistOnItemClick>
+                        <MenuTrigger>
+                            <Button appearance="subtle">Other tools</Button>
+                        </MenuTrigger>
+                        <MenuPopover>
+                            <MenuList>
+                                <MenuItemLink href="https://www.keyboard-layout-editor.com/" target="_blank">
+                                    Keyboard Layout Editor
+                                </MenuItemLink>
+                                <MenuItemLink href="https://nickcoutsos.github.io/keymap-layout-tools/" target="_blank">
+                                    Keymap Layout Tools
+                                </MenuItemLink>
+                                <MenuItemLink href="https://qmk.fm/converter/" target="_blank">
+                                    KLE 🡒 QMK JSON
+                                </MenuItemLink>
+                                <MenuItemLink
+                                    href="https://zmk-physical-layout-converter.streamlit.app/"
+                                    target="_blank"
+                                >
+                                    QMK JSON 🡒 Devicetree
+                                </MenuItemLink>
+                            </MenuList>
+                        </MenuPopover>
+                    </Menu>
+                    <Button
+                        as="a"
+                        href="https://github.com/joelspadin/zmk-layout-helper"
+                        target="_blank"
+                        appearance="subtle"
+                        icon={<GitHubIcon />}
+                    >
+                        GitHub
+                    </Button>
+                </div>
+            </div>
 
             <div className={classes.content}>
                 {tab === 'import' && <ImportPage onImport={() => setTab('positions')} />}
@@ -50,13 +100,25 @@ const PageContents: React.FC = () => {
 };
 
 const useStyles = makeStyles({
+    header: {
+        display: 'grid',
+        gridTemplate: 'auto / 1fr max-content 1fr',
+
+        backgroundColor: tokens.colorNeutralBackground1,
+        boxShadow: tokens.shadow4,
+        zIndex: 1,
+    },
     tabs: {
         display: 'flex',
         flexFlow: 'row wrap',
         justifyContent: 'center',
-        backgroundColor: tokens.colorNeutralBackground1,
-        boxShadow: tokens.shadow4,
-        zIndex: 1,
+    },
+    links: {
+        display: 'flex',
+        flexFlow: 'row wrap',
+        justifyContent: 'end',
+        alignItems: 'center',
+        paddingRight: tokens.spacingHorizontalM,
     },
     content: {
         height: 'calc(100vh - 44px)',
